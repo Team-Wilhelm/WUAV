7c7517d451178d5c2d92197bdb703648cf3059c4
Julia Ilasova <1julka1il@gmail.com>
2023-05-15 17:31:16 +0200

    the label appears to be floating

--- src/main/java/gui/nodes/TextAreaWithFloatingText.java
+++ src/main/java/gui/nodes/TextAreaWithFloatingText.java
@@ -16,12 +16,14 @@ import javafx.scene.text.Font;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Transform;
 import javafx.scene.transform.Translate;
+import org.w3c.dom.css.RGBColor;
 
 
 public class TextAreaWithFloatingText extends StackPane {
     private final TextArea textArea;
     private final Label floatingLabel;
     private final StringProperty floatingTextProperty = new SimpleStringProperty();
+    private final BooleanProperty isFloatingProperty = new SimpleBooleanProperty(false);
 
     public TextAreaWithFloatingText(String floatingText) {
         super();
@@ -29,87 +31,67 @@ public class TextAreaWithFloatingText extends StackPane {
 
         setMaxWidth(Double.MAX_VALUE);
         setMaxHeight(Double.MAX_VALUE);
+        floatingTextProperty().set(floatingText);
+        if (floatingText == null || floatingText.isEmpty()) {
+            isFloatingProperty.set(true);
+        }
 
         // TextArea
         textArea = new TextArea();
-        floatingTextProperty().set(floatingText);
+        textArea.setWrapText(true);
 
         // Label
         floatingLabel = new Label();
         floatingLabel.textProperty().bind(floatingTextProperty);
         floatingLabel.getStyleClass().add("floating-label");
 
-        // Bind the text area's height to the stack pane's height minus the label's height
-        textArea.prefHeightProperty().bind(this.heightProperty().subtract(floatingLabel.heightProperty()));
-
-        Bindings.createBooleanBinding(() -> !textArea.getText().isEmpty(), textArea.textProperty()).addListener((observable, oldValue, newValue) -> {
-            if (newValue) {
-                floatingLabel.setFont(Font.font(12));
-                //StackPane.setMargin(textArea, new Insets(floatingLabel.getHeight() + 5, 0, 0, 0));
-            } else {
-                floatingLabel.setFont(Font.font(14));
-                //StackPane.setMargin(textArea, new Insets(floatingLabel.getHeight() + 5, 0, 0, 0));
-            }
-        });
-
-        Bindings.createBooleanBinding(textArea::isFocused, textArea.focusedProperty()).addListener((observable, oldValue, newValue) -> {
-            if (newValue) {
-                floatingLabel.setFont(Font.font(12));
-            } else if (textArea.getText().isEmpty()) {
-                floatingLabel.setFont(Font.font(14));
-            }
-        });
-
         this.getChildren().addAll(textArea, floatingLabel);
         StackPane.setAlignment(floatingLabel, Pos.TOP_LEFT);
         StackPane.setAlignment(textArea, Pos.CENTER_LEFT);
-        //StackPane.setMargin(floatingLabel, new Insets(5, 0, 0, 5));
-        //StackPane.setMargin(textArea, new Insets(floatingLabel.getHeight() + 5, 0, 0, 0));
 
         textArea.prefWidthProperty().bind(this.widthProperty());
-        textArea.prefHeightProperty().bind(this.heightProperty());
-        //addListener();
+        textArea.maxHeightProperty().bind(this.heightProperty().subtract(floatingLabel.heightProperty()));
 
         this.setOnMouseClicked(e -> {
             if (!this.isFocused()) {
                 textArea.requestFocus();
             }
         });
-    }
 
-    private void addListener() {
-        textArea.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue) { // If text area is focused, make label float
-                floatingLabel.setFont(Font.font(12));
-            } else if (textArea.getText().isEmpty()) { // If text area is not focused and empty, make label sink
-                floatingLabel.setFont(Font.font(14));
-            }
+        this.borderProperty().bind(Bindings
+                .when(textArea.focusedProperty())
+                .then(new Border(new BorderStroke(Color.valueOf("0e283f"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))))
+                .otherwise(new Border(new BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)))));
+
+        // When the text area is focused or empty, the label should float above the text area
+        isFloatingProperty.bind(Bindings.createBooleanBinding(() -> textArea.isFocused()
+                || !textArea.getText().isEmpty(),
+                textArea.focusedProperty(),
+                textArea.textProperty()));
+
+        floatingLabel.fontProperty().bind(Bindings
+                .when(isFloatingProperty)
+                .then(Font.font(14))
+                .otherwise(Font.font(16)));
+
+        isFloatingProperty.addListener((observable, oldValue, newValue) -> {
+            updateFloatingLabel();
         });
-
-        textArea.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (!textArea.getText().isEmpty()) {
-                floatingLabel.setFont(Font.font(12));  // Set smaller font size
-            }
-        });
-
-        // Update the label's size based on its content
-        double labelWidth = computeLabelWidth();
-        double labelHeight = computeLabelHeight();
-        floatingLabel.resize(labelWidth, labelHeight);
     }
 
-    private double computeLabelWidth() {
-        double textWidth = textArea.getWidth();
-        double labelWidth = textArea.isFocused() ? floatingLabel.prefWidth(-1) : 0;
-        return Math.max(textWidth, labelWidth);
+    private void updateFloatingLabel() {
+        if (isFloatingProperty.get()) {
+            StackPane.setMargin(textArea, new Insets(floatingLabel.getHeight() + 4, 0, 0, 0));
+        } else {
+            StackPane.setMargin(textArea, Insets.EMPTY);
+        }
     }
 
-    private double computeLabelHeight() {
-        double textHeight = textArea.getHeight();
-        double labelHeight = textArea.isFocused() ? floatingLabel.prefHeight(-1) : 0;
-        return Math.max(textHeight, labelHeight);
+    private void setUpStackPane() {
+
     }
 
+    // region Getters and Setters
     public String getFloatingText() {
         return floatingTextProperty.get();
     }
@@ -121,4 +103,5 @@ public class TextAreaWithFloatingText extends StackPane {
     public StringProperty floatingTextProperty() {
         return floatingTextProperty;
     }
+    // endregion
 }
