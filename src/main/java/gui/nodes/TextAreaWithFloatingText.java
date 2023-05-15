5d540fb727eec9184b774457d675957c22ce61d5
Julia Ilasova <1julka1il@gmail.com>
2023-05-15 20:05:04 +0200

    minor changes

--- src/main/java/gui/nodes/TextAreaWithFloatingText.java
+++ src/main/java/gui/nodes/TextAreaWithFloatingText.java
@@ -1,40 +1,46 @@
 package gui.nodes;
 
-import io.github.palexdev.materialfx.beans.PositionBean;
-import io.github.palexdev.materialfx.utils.PositionUtils;
+import io.github.palexdev.materialfx.utils.AnimationUtils;
+import javafx.animation.Animation;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.*;
-import javafx.geometry.HPos;
+import javafx.css.*;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.scene.transform.Scale;
-import javafx.scene.transform.Transform;
-import javafx.scene.transform.Translate;
-import org.w3c.dom.css.RGBColor;
+
+import javax.swing.text.html.StyleSheet;
 
 
 public class TextAreaWithFloatingText extends StackPane {
+    // Child nodes
     private final TextArea textArea;
     private final Label floatingLabel;
+
+    // Properties
     private final StringProperty floatingTextProperty = new SimpleStringProperty();
     private final BooleanProperty isFloatingProperty = new SimpleBooleanProperty(false);
+    private int fontSize;
+
+    //private static final StyleablePropertyFactory<TextAreaWithFloatingText> FACTORY = new StyleablePropertyFactory<>(TextAreaWithFloatingText.getClassCssMetaData());
+    //private final StyleableProperty<Font> fontProperty;
+    private Border unfocusedBorder, focusedBorder;
+    private Animation floatingAnimation;
 
     public TextAreaWithFloatingText(String floatingText) {
         super();
-        getStyleClass().add("text-area-with-floating-text");
+        getStyleClass().setAll("text-area-with-floating-text");
+        getStylesheets().setAll("/css/style.css");
 
-        setMaxWidth(Double.MAX_VALUE);
-        setMaxHeight(Double.MAX_VALUE);
-        floatingTextProperty().set(floatingText);
-        if (floatingText == null || floatingText.isEmpty()) {
-            isFloatingProperty.set(true);
-        }
+        // StackPane
+        setUpStackPane(floatingText);
+
+        // Get font from style.css
+        //fontProperty = new SimpleStyleableObjectProperty<>(FACTORY.createFontCssMetaData("-fx-font", s -> s.fontProperty, Font.getDefault(), false));
 
         // TextArea
         textArea = new TextArea();
@@ -49,23 +55,55 @@ public class TextAreaWithFloatingText extends StackPane {
         StackPane.setAlignment(floatingLabel, Pos.TOP_LEFT);
         StackPane.setAlignment(textArea, Pos.CENTER_LEFT);
 
-        textArea.prefWidthProperty().bind(this.widthProperty());
-        textArea.maxHeightProperty().bind(this.heightProperty().subtract(floatingLabel.heightProperty()));
-
         this.setOnMouseClicked(e -> {
             if (!this.isFocused()) {
                 textArea.requestFocus();
+                textArea.positionCaret(textArea.getText().length());
             }
         });
+        createBindings();
 
+        isFloatingProperty.addListener((observable, oldValue, newValue) -> {
+            resizeChildren();
+        });
+    }
+
+    private void resizeChildren() {
+        StackPane.setMargin(textArea, new Insets(floatingLabel.getHeight() + 5, 0, 0, 0));
+    }
+
+    private void setUpStackPane(String floatingText) {
+        setMaxWidth(Double.MAX_VALUE);
+        setMaxHeight(Double.MAX_VALUE);
+        floatingTextProperty().set(floatingText);
+        if (floatingText == null || floatingText.isEmpty()) {
+            isFloatingProperty.set(true);
+        }
+
+        // Border
+        unfocusedBorder = new Border(
+                new BorderStroke(Color.LIGHTGRAY,
+                        BorderStrokeStyle.SOLID,
+                        new CornerRadii(0.5),
+                        new BorderWidths(1.5)));
+        focusedBorder = new Border(
+                new BorderStroke
+                (Color.valueOf("0e283f"),
+                        BorderStrokeStyle.SOLID,
+                        new CornerRadii(0.5),
+                        new BorderWidths(1.5)));
+    }
+
+    private void createBindings() {
+        // Border color
         this.borderProperty().bind(Bindings
                 .when(textArea.focusedProperty())
-                .then(new Border(new BorderStroke(Color.valueOf("0e283f"), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))))
-                .otherwise(new Border(new BorderStroke(Color.LIGHTGRAY, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)))));
+                .then(focusedBorder)
+                .otherwise(unfocusedBorder));
 
-        // When the text area is focused or empty, the label should float above the text area
+        // When the text area is focused or empty, the label should become larger
         isFloatingProperty.bind(Bindings.createBooleanBinding(() -> textArea.isFocused()
-                || !textArea.getText().isEmpty(),
+                        || !textArea.getText().isEmpty(),
                 textArea.focusedProperty(),
                 textArea.textProperty()));
 
@@ -74,22 +112,9 @@ public class TextAreaWithFloatingText extends StackPane {
                 .then(Font.font(14))
                 .otherwise(Font.font(16)));
 
-        isFloatingProperty.addListener((observable, oldValue, newValue) -> {
-            updateFloatingLabel();
-        });
+        textArea.maxWidthProperty().bind(this.widthProperty().subtract(5));
     }
 
-    private void updateFloatingLabel() {
-        if (isFloatingProperty.get()) {
-            StackPane.setMargin(textArea, new Insets(floatingLabel.getHeight() + 4, 0, 0, 0));
-        } else {
-            StackPane.setMargin(textArea, Insets.EMPTY);
-        }
-    }
-
-    private void setUpStackPane() {
-
-    }
 
     // region Getters and Setters
     public String getFloatingText() {
@@ -104,4 +129,5 @@ public class TextAreaWithFloatingText extends StackPane {
         return floatingTextProperty;
     }
     // endregion
+
 }
