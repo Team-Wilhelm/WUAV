4c8efd9ff8395e2ffb7531e8c454ea1780c1ae7b
Matej Mazúr <matejmazur@outlook.com>
2023-05-24 12:24:25 +0200

    Add a splash screen on startup.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,63 @@
-import be.User;
-import gui.util.DialogManager;
 import gui.util.SceneManager;
-import gui.model.UserModel;
 import io.github.palexdev.materialfx.css.themes.MFXThemeManager;
 import io.github.palexdev.materialfx.css.themes.Themes;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import utils.ThreadPool;
 
+import java.io.IOException;
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
 
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(SceneManager.LOGIN_SCENE)));
-        primaryStage.setTitle("WUAV Documentation Management System");
-        primaryStage.getIcons().add(new Image("/img/icons/WUAV.png"));
-
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
-
-        primaryStage.centerOnScreen();
+        Parent splashRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/Splash.fxml")));
+        Scene splashScene = new Scene(splashRoot);
+        primaryStage.setScene(splashScene);
         primaryStage.show();
 
-        // All tables always visible
-        //primaryStage.setMinWidth(1310);
+        PauseTransition splashDelay = new PauseTransition(Duration.seconds(1));
+        splashDelay.setOnFinished(event -> {
+            try {
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(SceneManager.LOGIN_SCENE)));
+                primaryStage.setTitle("WUAV Documentation Management System");
+                primaryStage.getIcons().add(new Image("/img/icons/WUAV.png"));
 
-        primaryStage.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
-            if (e.getClickCount() == 2 && primaryStage.isMaximized() && e.getScreenY() < 30) {
-                primaryStage.setMaximized(false);
-                primaryStage.setWidth(Screen.getPrimary().getBounds().getWidth() - 200);
-                primaryStage.setHeight(Screen.getPrimary().getBounds().getHeight() - 200);
+                Scene scene = new Scene(root);
+                System.out.println("Loading application");
+                primaryStage.setScene(scene);
+                MFXThemeManager.addOn(scene, Themes.DEFAULT, Themes.LEGACY);
+
                 primaryStage.centerOnScreen();
-            } else if (e.getClickCount() == 2 && !primaryStage.isMaximized())
-                primaryStage.setMaximized(true);
+                primaryStage.show();
+
+                // All tables always visible
+                //primaryStage.setMinWidth(1310);
+
+                primaryStage.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+                    if (e.getClickCount() == 2 && primaryStage.isMaximized() && e.getScreenY() < 30) {
+                        primaryStage.setMaximized(false);
+                        primaryStage.setWidth(Screen.getPrimary().getBounds().getWidth() - 200);
+                        primaryStage.setHeight(Screen.getPrimary().getBounds().getHeight() - 200);
+                        primaryStage.centerOnScreen();
+                    } else if (e.getClickCount() == 2 && !primaryStage.isMaximized())
+                        primaryStage.setMaximized(true);
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
+
+        splashDelay.play();
     }
 
     public static void main(String[] args) {
